b9c1584a9c9836bd9767dd817ba1485b1fd268f4
Nicklas Utgaard <nutgaard@gmail.com>
2012-08-30 22:51:55 +0200

    Fixed androidmanifest to correctly start the right activity. MainActivity added.

---
+++ kpro-app/src/main/java/no/ntnu/kpro/core/model/Model.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package no.ntnu.kpro.core.model;
+
+import android.os.Parcelable;
+
+/**
+ *
+ * @author Nicklas
+ */
+public interface Model extends Parcelable {
+    
+}
