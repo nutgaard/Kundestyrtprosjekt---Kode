47315981f55f561c3dc481f2df44ada9955befb3
Nicklas Utgaard <nutgaard@gmail.com>
2012-09-05 16:02:50 +0200

    Start of connection between ActivityWrapper and ServiceProvider, still needs Work

--- kpro-app/src/main/java/no/ntnu/kpro/app/WrapperActivity.java
+++ kpro-app/src/main/java/no/ntnu/kpro/app/WrapperActivity.java
@@ -5,11 +5,39 @@
 package no.ntnu.kpro.app;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import no.ntnu.kpro.core.service.ServiceProvider;
+import org.omg.CosNaming.Binding;
 
 /**
  *
  * @author Nicklas
  */
 public class WrapperActivity extends Activity {
-    
+    private ServiceProvider serviceProvider;
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        public void onServiceConnected(ComponentName cn, IBinder ib) {
+            ServiceProvider.LocalBinder lb = (ServiceProvider.LocalBinder)ib;
+            serviceProvider = lb.getService();
+        }
+
+        public void onServiceDisconnected(ComponentName cn) {
+            serviceProvider = null;
+        }
+    };
+    public ServiceProvider getServiceProvider() {
+        return serviceProvider;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Needs some intent to bind to serice
+//        bindService(new Intent(Binding.this, ServiceProvider.class), mConnection, Context.BIND_AUTO_CREATE);
+    }
 }
--- kpro-app/src/main/java/no/ntnu/kpro/core/service/ServiceProvider.java
+++ kpro-app/src/main/java/no/ntnu/kpro/core/service/ServiceProvider.java
@@ -6,10 +6,9 @@ package no.ntnu.kpro.core.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
-import java.util.Timer;
-import java.util.TimerTask;
 
 /**
  *
@@ -17,36 +16,31 @@ import java.util.TimerTask;
  */
 public class ServiceProvider extends Service {
 
-//    private static final String TAG = ServiceProvider.class.getSimpleName();
+    private IBinder mBinder = new LocalBinder();
     private static final String TAG = "KPRO";
-    private Timer timer;
-    private TimerTask updateTask = new TimerTask() {
-
-        @Override
-        public void run() {
-            Log.i(TAG, "Timer task doing work");
-        }
-    };
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
     
     @Override
     public void onCreate() {
         super.onCreate();
         Log.i(TAG, "Service starting");
-        
-        timer = new Timer("TweetCollectorTimer");
-        timer.schedule(updateTask, 1000L, 5 * 1000L);
     }
     
     @Override
     public void onDestroy() {
         super.onDestroy();
         Log.i(TAG, "Service destroyed");
-        timer.cancel();
-        timer = null;
+    }
+    
+    @Override
+    public IBinder onBind(Intent intent) {
+        if (this.mBinder == null){
+            
+        }
+        return this.mBinder;
+    }
+    public class LocalBinder extends Binder {
+        public ServiceProvider getService(){
+            return ServiceProvider.this;
+        }
     }
 }
