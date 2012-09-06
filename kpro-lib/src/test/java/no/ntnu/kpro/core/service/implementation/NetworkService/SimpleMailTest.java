2b8b04fcc49ca199e37888cfa094c898296d23b0
Nicklas Utgaard <nutgaard@gmail.com>
2012-09-06 15:39:57 +0200

    Added static test classes for IMAPS

--- kpro-lib/src/test/java/no/ntnu/kpro/core/service/implementation/NetworkService/SimpleMailTest.java
+++ kpro-lib/src/test/java/no/ntnu/kpro/core/service/implementation/NetworkService/SimpleMailTest.java
@@ -1,20 +1,26 @@
 package no.ntnu.kpro.core.service.implementation.NetworkService;
 
+import com.icegreen.greenmail.user.GreenMailUser;
 import com.icegreen.greenmail.util.DummySSLSocketFactory;
 import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.ServerSetup;
 import com.icegreen.greenmail.util.ServerSetupTest;
 import java.security.Security;
 import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import org.junit.After;
 import static org.junit.Assert.*;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({SimpleMailTest.Mock.class, SimpleMailTest.Real.class})
+@Suite.SuiteClasses({SimpleMailTest.SMTPSMock.class, SimpleMailTest.IMAPSMock.class, SimpleMailTest.SMTPSReal.class, SimpleMailTest.IMAPSReal.class})
 public class SimpleMailTest {
 
     private static final String USER_PASSWORD = "kprothales2012";
@@ -23,9 +29,11 @@ public class SimpleMailTest {
     private static final String EMAIL_TO = "kprothales@gmail.com";
     private static final String EMAIL_SUBJECT = "Test E-Mail";
     private static final String EMAIL_TEXT = "This is a test e-mail.";
-    private static final String LOCALHOST = "127.0.0.1";;
+    private static final String LOCALHOST = "127.0.0.1";
 
-    public static class Mock {
+    ;
+
+    public static class SMTPSMock {
 
         private GreenMail mailServer;
         private SimpleMail mailClient;
@@ -51,6 +59,7 @@ public class SimpleMailTest {
         @After
         public void tearDown() {
             this.mailServer.stop();
+            this.mailClient = null;
 //            System.out.println("Teardown complete");
         }
 
@@ -71,9 +80,58 @@ public class SimpleMailTest {
 //            System.out.println("Test: getMails Complete");
         }
     }
-    public static class Real {
+
+    public static class IMAPSMock {
+
+        private GreenMail mailServer;
         private SimpleMail mailClient;
-        
+        private Properties props;
+        private GreenMailUser user;
+
+        @Before
+        public void setup() {
+            Security.setProperty("ssl.SocketFactory.provider", DummySSLSocketFactory.class.getName());
+            this.props = new Properties();
+            this.props.put("mail.imaps.host", LOCALHOST);
+            this.props.put("mail.imaps.auth", "true");
+            this.props.put("mail.imaps.port", ServerSetupTest.IMAPS.getPort());
+            
+            this.mailServer = new GreenMail(ServerSetup.IMAPS);
+            this.mailServer.start();
+            this.user = this.mailServer.setUser(EMAIL_USER_ADDRESS, USER_NAME, USER_PASSWORD);
+            
+            this.mailClient = new SimpleMail(USER_NAME, USER_PASSWORD, EMAIL_USER_ADDRESS);
+            this.mailClient.setProps(props);
+            this.mailClient.setAuth(null);
+        }
+
+        @After
+        public void teardown() {
+            this.mailServer.stop();
+            this.mailClient = null;
+        }
+
+        @Test
+        public void fetchMail_Mock() throws Exception {
+            //Test message
+            MimeMessage message = new MimeMessage((Session)null);
+            message.setFrom(new InternetAddress(EMAIL_USER_ADDRESS));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(EMAIL_TO));
+            message.setSubject(EMAIL_SUBJECT);
+            message.setText(EMAIL_TEXT);
+            
+            //Use greenmailuser to deliver message
+            this.user.deliver(message);
+            
+            //Fetch using SimpleMail IMAPS
+            
+        }
+    }
+
+    public static class SMTPSReal {
+
+        private SimpleMail mailClient;
+
         @Before
         public void setup() {
             this.mailClient = new SimpleMail(USER_NAME, USER_PASSWORD, EMAIL_USER_ADDRESS);
@@ -84,10 +142,26 @@ public class SimpleMailTest {
         public void tearDown() {
 //            System.out.println("Teardown complete");
         }
+
         @Test
         public void sendMail_Real() {
             boolean b = mailClient.sendMail(EMAIL_TO, EMAIL_SUBJECT, EMAIL_TEXT);
             assertTrue(b);
+        }
+    }
+
+    public static class IMAPSReal {
+
+        @Before
+        public void setup() {
+        }
+
+        @After
+        public void teardown() {
+        }
+
+        @Test
+        public void fetchMail_Real() {
         }
     }
 }
