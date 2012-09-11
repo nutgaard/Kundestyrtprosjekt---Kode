bb8bc85597de33dd8ec98295fd555bbeb8402e88
Nicklas Utgaard <nutgaard@gmail.com>
2012-09-11 11:30:39 +0200

    Added XOMessage

---
+++ kpro-app/src/main/java/no/ntnu/kpro/core/model/XOMessagePriority.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package no.ntnu.kpro.core.model;
+
+/**
+ *
+ * @author Nicklas
+ */
+public enum XOMessagePriority {
+    NONE;
+}
