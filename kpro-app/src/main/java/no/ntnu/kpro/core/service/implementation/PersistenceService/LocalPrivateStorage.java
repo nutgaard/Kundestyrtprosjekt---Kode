544e6214f6c5a5257445b084d4af90048dcd27c6
Gnaskar <magnus.ulstein@gmail.com>
2012-09-12 14:05:56 +0200

    Just to  avoid mergeconf

--- kpro-app/src/main/java/no/ntnu/kpro/core/service/implementation/PersistenceService/LocalPrivateStorage.java
+++ kpro-app/src/main/java/no/ntnu/kpro/core/service/implementation/PersistenceService/LocalPrivateStorage.java
@@ -6,6 +6,7 @@ package no.ntnu.kpro.core.service.implementation.PersistenceService;
 
 import android.content.Context;
 import android.util.Log;
+import com.thoughtworks.xstream.XStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -18,26 +19,17 @@ import no.ntnu.kpro.core.service.interfaces.PersistenceService;
  *
  * @author aleksandersjafjell
  */
-public class LocalPrivateStorage implements PersistenceService {
+public class LocalPrivateStorage extends PersistenceService {
 
-    /**
-     * Saves a new file if it does not exist, and overwrites old if it does.
-     * @param fileName Name of file to be saved.
-     * @param content File content.
-     * @returns true if file saved successfully, else false.
-     */
-    public boolean saveToStorage(String fileName, String content, Context context) {
-        try{
-            FileOutputStream fos = context.openFileOutput(fileName, Context.MODE_PRIVATE);
-            fos.write(content.getBytes());
-            fos.close();
-            return true;
-        }catch (java.io.IOException e){
-            Log.d("LocalPrivateStorage", e.getStackTrace().toString());
-            return false;
-        }
-    }    
-    public boolean saveToStorage(String fileName, String folder, String content, Context context) {
+    XStream xs;
+    Context c;
+    
+    public LocalPrivateStorage(Context context) {
+        xs = new XStream();
+        c = context;
+    }
+      
+    private boolean saveToStorage(String fileName, String folder, String content) {
         try{
             File file = new File(folder, fileName);
             FileOutputStream fos = new FileOutputStream(file);
@@ -55,19 +47,10 @@ public class LocalPrivateStorage implements PersistenceService {
      * @param fileName
      * @return 
      */
-    public BufferedReader readFromStorage(String fileName) {
+   
+    private BufferedReader readFromStorage(String fileName, String folder) {
         try {
-            FileReader fr = new FileReader(fileName);
-            BufferedReader output = new BufferedReader(fr);
-            return output;
-        } catch (FileNotFoundException e) {
-           Log.d("LocalPrivateStorage", e.getStackTrace().toString());
-        }
-        return null;
-    }
-    public BufferedReader readFromStorage(String fileName, String folder, Context context) {
-        try {
-            File dir = context.getDir(folder, context.MODE_PRIVATE);
+            File dir = c.getDir(folder, c.MODE_PRIVATE);
             File file = new File(dir, fileName);
             FileReader fr = new FileReader(file);
             BufferedReader output = new BufferedReader(fr);
@@ -79,7 +62,7 @@ public class LocalPrivateStorage implements PersistenceService {
     }
 
 
-    public boolean isAuthorized() {
+    private boolean isAuthorized() {
         return true;
     }
     
@@ -88,10 +71,47 @@ public class LocalPrivateStorage implements PersistenceService {
      * @param fileName name of the file to delete
      * @return true if the file was successfully deleted, else false.
      */
-    public boolean removeFile(String fileName, Context context) {
+    private boolean removeFile(String fileName, Context context) {
         return context.deleteFile(fileName);
     }
-
+    
+    private boolean saveFile(Object fileToSave, String dir, String fileName){
+        String data = xs.toXML(fileToSave);
+        return saveToStorage(fileName, dir, data);
+    }
+    
+    private Object loadFile(String fileName){
+        try{
+            BufferedReader br = readFromStorage(fileName,"");
+            String nextLine = br.readLine();
+            String xml = null;
+            while (nextLine != null){
+                xml = xml + nextLine;
+                nextLine = br.readLine();
+        }
+            return xs.fromXML(xml);
+        }catch(Exception e){
+            Log.d("StorageWrpper", e.getStackTrace().toString());
+            return null;
+        }
+    }
+    /**
+     * provides a list of all existing files in the base folder
+     * @return 
+     */
+    private String[] getFileList(){
+        File dir = c.getFilesDir();
+        return dir.list();
+    }
+    private String[] fileSearch(FilenameFilter filter){
+        File dir = c.getFilesDir();
+        return dir.list(filter);
+    }
+    /*****************************************************
+     *          HERE COMES THE OUTSIDE METHODS
+     *****************************************************/
+    
+    
     public void save(String fileName, Object objectToSave) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
