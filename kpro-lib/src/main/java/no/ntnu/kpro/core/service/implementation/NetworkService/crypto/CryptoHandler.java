8ef4d7d8f8eabbd9e93769ad0cc0024c22851ba1
Gnaskar <magnus.ulstein@gmail.com>
2012-10-16 10:47:02 +0200

    preparing to push

--- kpro-lib/src/main/java/no/ntnu/kpro/core/service/implementation/NetworkService/crypto/CryptoHandler.java
+++ kpro-lib/src/main/java/no/ntnu/kpro/core/service/implementation/NetworkService/crypto/CryptoHandler.java
@@ -5,9 +5,16 @@
 package no.ntnu.kpro.core.service.implementation.NetworkService.crypto;
 
 import java.security.Security;
+import java.util.Properties;
 import javax.activation.CommandMap;
 import javax.activation.MailcapCommandMap;
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import no.ntnu.kpro.core.model.XOMessage;
 import org.spongycastle.jce.provider.BouncyCastleProvider;
 
@@ -28,7 +35,42 @@ public class CryptoHandler {
        
     
     public MimeMessage prepareToSend(XOMessage mail){
+        try {
+        //SMIMESignedGenerator gen = new SMIMESignedGenerator();
+        //gen.addCertificates(certs);
+
+        //
+        // create the base for our message
+        //
+        MimeBodyPart msg = new MimeBodyPart();
+
+        msg.setText(mail.getStrippedBody()); //TODO: consider adding support for HTML
+
+        //
+        // extract the multipart object from the SMIMESigned object.
+        //
+        //MimeMultipart mm = gen.generate(msg);
+
+        //
+        // Get a Session object and create the mail message
+        //
+        Properties props = System.getProperties();
+        Session session = Session.getDefaultInstance(props, null);
+
+        Address fromUser = new InternetAddress(mail.getFrom());
+        Address toUser = new InternetAddress(mail.getTo());
+
+        MimeMessage body = new MimeMessage(session);
+        body.setFrom(fromUser);
+        body.setRecipient(Message.RecipientType.TO, toUser);
+        body.setSubject(mail.getSubject());
+        //body.setContent(mm, mm.getContentType());
+        body.saveChanges();
         return null;
+        }
+        catch(Exception e){
+            return null;
+        }
     }
     public XOMessage decrypt(MimeMessage mail){
         return null;
